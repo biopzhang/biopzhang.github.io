b427a9d374a0248024cd78829bd375d7a63493eb
Pengzhi Zhang <pzhang.uh@gmail.com>
2026-06-04 11:57:15 +0800

    update publications style

--- _plugins/hideCustomBibtex.rb
+++ _plugins/hideCustomBibtex.rb
@@ -1,13 +1,21 @@
- module Jekyll
+module Jekyll
   module HideCustomBibtex
     def hideCustomBibtex(input)
-	  keywords = @context.registers[:site].config['filtered_bibtex_keywords']
+      keywords = @context.registers[:site].config['filtered_bibtex_keywords']
 
-	  keywords.each do |keyword|
-		input = input.gsub(/^.*#{keyword}.*$\n/, '')
-	  end
+      keywords.each do |keyword|
+        input = input.gsub(/^.*#{keyword}.*$\n/, '')
+      end
 
-      return input
+      strip_author_markers(input)
+    end
+
+    private
+
+    def strip_author_markers(input)
+      input.gsub(/^(.*author\s*=\s*\{.*)$/i) do |line|
+        line.gsub(/[\u2020\u00b6]/, '').gsub(/([[:alpha:]])[*&^](?=[,\s}])/, '\1')
+      end
     end
   end
 end
